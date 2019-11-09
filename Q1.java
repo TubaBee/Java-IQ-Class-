ce8f6099b444df22eb04c79989ce40b6382371a4
tubaBee <tuba.onslbee@gmail.com>
2019-11-09 14:30:12 -0500

    Interview soruve cevaplar 1 eklendi

--- Q1.java
+++ Q1.java
@@ -1,19 +1,43 @@
 package JavaIQProgQuestion;
 
+import java.util.Arrays;
+
 public class Q1 {
 
 	public static void main(String[] args) {
 	/*1-Write a program to swap 2 numbers without a temporary variable?
 	 *  Swap  2 strings without a temporary variable?	
-	 */System.out.println(".........1........");
-		int a=11,b=22;
-		int temp;
-		a=b;
-		System.out.println(a);
+//	 */System.out.println(".........1........");
+//		int a=11,b=22;
+//		int temp;
+//		a=b;
+//		System.out.println(a);
+//		
+//		String c="tuba",d="Ayse";
+//		c=d;
+//		System.out.println(c);
 		
-		String c="tuba",d="Ayse";
-		c=d;
-		System.out.println(c);
+	//hocanin cozumu
+		
+		int a=10;
+		int b=20;
+		a=a+b; //30
+		b=a-b;//30-20=10
+		a=a-b; //30-10=20
+		System.out.println("The value of a="+a);
+		System.out.println("The value of b="+b);
+		
+		//2 String method
+		String str11="Hello";
+		String str22="Welcome";
+		
+		str11=str11+str22; //HelloWelcome
+		str22=str11.substring(0,str11.length()-str22.length()); //Hello
+		str11=str11.substring(str22.length()); 
+		
+		System.out.println("The value of str11="+str11);
+		System.out.println("The value of str22="+str22);
+		
 		
 	/*2-Write a java program to find the second largest number 
 	 * in the array? 
@@ -21,60 +45,103 @@ public class Q1 {
 	 */
 		System.out.println("..........2..........");
 		
-		int[] array={12,22,44,13,54,32,66,82,31,36};
+//		int[] array={12,22,44,13,54,32,66,82,31,36};
+//		
+//		int max1=array[0];
+//		int smax=array[0];
+//		for (int j = 0; j < array.length; j++) {
+//				
+//			if(max1<array[j] ) {
+//				max1=array[j];
+//			}
+//				if (array[j]<smax || smax<max1) {
+//					smax=array[j];
+//				}
+//			
+//			}System.out.println(max1);
+//		System.out.println(smax);
 		
-		int max1=array[0];
-		int smax=array[0];
-		for (int j = 0; j < array.length; j++) {
-				
-			if(max1<array[j] ) {
-				max1=array[j];
+		//Hocanin cozumu 
+		
+		int [] array= {100,-90,8787,898,0,1};
+		//1easiest way
+		Arrays.sort(array);
+		int min=array[0];
+		int max=array[array.length-1];
+		System.out.println(min);
+		System.out.println(max);
+		//2 way
+		int[] myArray= {100,-90,8787,898,0,1};
+		int largest=myArray[0];
+		int smalest=myArray[0];
+		for (int i = 0; i < myArray.length; i++) {
+			if(myArray[i]>largest) {
+				largest=myArray[i];
 			}
-				if (array[j]<smax || smax<max1) {
-					smax=array[j];
-				}
+			if(myArray[i]<smalest){
+				smalest=myArray[i];
+			}
+		}
+		System.out.println("The smalest value in the array is "+smalest);
+		System.out.println("The largest value in the array is "+largest);
+		
+		//
+		int[] nums= {100,-90,8787,898,0,1};
+		int large=nums[0];
+		int slarge=nums[0];
+		
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i]>large ) {
+				slarge=large;
+				large=nums[i];
+			}
 			
-			}System.out.println(max1);
-		System.out.println(smax);
+			if(nums[i]>slarge && nums[i]<large) {
+				slarge=nums[i];
+			}
+			
+		}
+		System.out.println("Large number"+large);
+		System.out.println("Second large number"+slarge);
 		
 	//.........................baskasinin yaptigi cozum..................
 		 int [] myNums= { 10, 212, 44, 35, 315, 404, 12, 9};
 		    
-		   int max=myNums[0]; 
+		   int max11=myNums[0]; 
 		   int secondMax=myNums[0];
 		   
 		    
 		   
 		   for (int i = 0; i < myNums.length; i++) {
-		        if (myNums[i]>max) {
-		            max=myNums[i]; 
+		        if (myNums[i]>max11) {
+		            max11=myNums[i]; 
 		        }
 		    }   
 		    
 		   
 		   
-		   int min=max; 
+		   int min11=max11; 
 		   for (int i = 0; i < myNums.length; i++) {
-		    if (myNums[i]<min) {
-		        min=myNums[i];
+		    if (myNums[i]<min11) {
+		        min11=myNums[i];
 		    }
 		}
 		  
 		   for (int i = 0; i < myNums.length; i++) {
 		       
-		       if (myNums[i]>secondMax&&myNums[i]!=max) {
+		       if (myNums[i]>secondMax&&myNums[i]!=max11) {
 		           
 		           secondMax=myNums[i]; 
 		           
 		       }
 		   }
-		   System.out.println("The max number is :"+max);
+		   System.out.println("The max number is :"+max11);
 		   
 		   
 		   System.out.println("The second max number is :"+secondMax);
 		   
 		   
-		   System.out.println("The minx number is :"+min);
+		   System.out.println("The minx number is :"+min11);
 		   
 		
 		
@@ -83,8 +150,8 @@ public class Q1 {
 	/*3-Find out how many alpha characters present in a string? */
 		System.out.println(".....3............");
 		String str="This*is$search#any!alpa@carachter";
-		System.out.println(str.replaceAll("[^A-Za-z0-9]", "").length());
-		
+		System.out.println(str.replaceAll("[^A-Za-z]", "").length());
+		//yada .replace("\\w",""); de yazilabilir yada .replace("[^a-n],"");
 	
 	/*4-How to find out the part of the string from a string?
 	 *  What is substring? 
@@ -92,12 +159,19 @@ public class Q1 {
 	 */
 		System.out.println(".....4.....");
 		
-		String str2="This is java program";
+//		String str2="This is java program";
+//		
+//		System.out.println(str2.substring(8)); 
+//		System.out.println(str2.length()); benim yine soruyu yanlis anlaisim
 		
-		System.out.println(str2.substring(8));
-		System.out.println(str2.length());
 		
+		/*Step1: split based on the space--> array of String
+		 * Step2: find the length of array
+		 */
 	
+		String str3="Today is very cold outside ";
+		String[] words=str3.split(" ");
+		System.out.println(words.length);
 		
 	}
 
--- Q1_1.java
+++ Q1_1.java
@@ -6,9 +6,11 @@ public class Q1_1 {
 		/*
 		 * 5-Write a java program to reverse String? Reverse a string word by word?
 		 */
+
 		System.out.println(".....5.......");
+
 		String str = "This is java program";
-		// String[] arr=str.split(" ");
+		String[] arr = str.split(" ");
 
 		for (int i = str.length() - 1; i >= 0; i--) {
 
@@ -23,24 +25,81 @@ public class Q1_1 {
 		for (int i = array.length - 1; i >= 0; i--) {
 			System.out.print(array[i] + " ");
 		}
+		System.out.println();
 
-		System.out.println("......6.......");
+// ........hocanin cozumu..........................
+		System.out.println(".....Hocanin cozumu............");
+		String given = "Welcome to the java class";
+		/*
+		 * to reverse String split(); Step1: split--> array of String Step2: use for
+		 * loop and use decrement to print values
+		 */
+
+//		String[] str11=given.split("\\s");
+//		for (int i= str11.length-1;  i>= 0; i--) {
+//			System.out.print(str11[i]+" ");
+//		}  // split ile yapimi
+
+		String reversed = "";
+		String[] str11 = given.split("\\s");
+		for (int i = str11.length - 1; i >= 0; i--) {
+			reversed = reversed + str11[i] + " ";
+		}
+		System.out.println(reversed);
+
+		// This is write a java program to reverse String?
+		// otCharArray(); charAt();
+
+		char[] charArray = given.toCharArray();
+		for (int i = charArray.length - 1; i >= 0; i--) {
+			System.out.print(charArray[i]);
+		}
+		String word3 = "I love Java";
+		for (int i = word3.length() - 1; i >= 0; i--) {
+			System.out.print(word3.charAt(i));
+		}
+
+		System.out.println("\n......6.......");
+
 		/*
 		 * 6-Write a Java Program to find whether a String is palindrome or not?
 		 */
 
-		String str1 = "fatma";
-		String a2=str1.substring(0,5);
+//		String str1 = "fatma";
+//		String a2=str1.substring(0,5);
+//
+//		for (int i= str1.length(); i >= 0;  i--) {
+//			if (a2.equals(str1)) {
+//			System.out.println(" \nThis is a palindrom word "+str1);
+//		}else {
+//			System.out.println("\nThis is not palindrom word");
+//		}
+//		}			
+//		 System.out.println(a2);
 
-		for (int i= str1.length(); i >= 0;  i--) {
-			if (a2.equals(str1)) {
-			System.out.println(" \nThis is a palindrom word "+str1);
-		}else {
-			System.out.println("\nThis is not palindrom word");
+//..........Hocanin cozumu.................
+		/*
+		 * Logical: 
+		 * Step1:reverse String 
+		 * Step2: compare 2 String:
+		 * 		if string are equels 
+		 * 
+		 */
+
+		String original = "kayak";
+		String reversd = "";
+		for (int i = original.length() - 1; i >= 0; i--) {
+			reversd = reversd + original.charAt(i);// +k=k+a=ka+k
 		}
-		}			
-		 System.out.println(a2);
+		System.out.println(reversd);
 
+		if (original.equals(reversd)) {
+			System.out.println("String is a palindrom");
+
+		} else {
+			System.out.println("String is not palindrom");
+		}
+		
 		System.out.println(".......7.......");
 		/*
 		 * 7-Write a java program to check whether a given number is prime or not?
@@ -48,30 +107,70 @@ public class Q1_1 {
 
 		int[] num = { 1, 3, 5, 7, 8, 9, 11, 12, 13 };
 
-		for (int i = 0; i < num.length; i++) {
-			int c = num.length;
-			if (num[i] % c == 1) {
-				System.out.println(num[i]);
-				c++;
+//		for (int i = 0; i < num.length; i++) {
+//			int c = num.length;
+//			if (num[i] % c == 1) {
+//				System.out.println(num[i]);
+//				c++;
+//			}
+//		}
+
+//...........hocanin cozumu ............................
+		
+		//take range of numbers from 2 to 100 and print all prime
+		//2,3 (2,3), 4(2,3,4), 5(2,3,4,5), 6(2,3,4,5,6)
+
+		
+		int num1=5;
+		boolean isPrime=true;
+		if(num1<=1) {
+			isPrime=false;
+	}else {
+		
+		for (int i = 2; i < num1; i++) {
+			if (num1%i==0) {
+				isPrime=false;
+				break;
 			}
 		}
-
+	}
+		
+		if(isPrime) {
+			System.out.println(num1+" is a prime number");
+		}else {
+			System.out.println(num1+" is NOT Prime number");
+		}
+		
+		
+		
 		System.out.println("......8.......");
 		/*
 		 * 8-Write a Java Program to print first 10 numbers of Fibonacci series.
 		 */
-		int[] a = new int[10];
-		a[0] = 0;
-		a[1] = 1;
-		int fib = 0;
-		for (int i = 2; i < a.length; i++) {
-			// for (int j = 0; j < 10; j++) {
-
-			System.out.println(a[i] = (a[i - 2]) + (a[i - 1]));
-
-			// }
+//		int[] a = new int[10];
+//		a[0] = 0;
+//		a[1] = 1;
+//		int fib = 0;
+//		for (int i = 2; i < a.length; i++) {
+//			// for (int j = 0; j < 10; j++) {
+//
+//			System.out.println(a[i] = (a[i - 2]) + (a[i - 1]));
+//
+//			// }
+		
+//...........hocanin cozumu.........................
+		int a,b,c;
+		a=0;
+		b=1;
+		for (int i = 0; i <10; i++) {
+			System.out.print(a+" ");
+			c=a+b;
+			a=b;
+			b=c;
+		}
+		
+		
+		
 		}
 
 	}
-
-}
